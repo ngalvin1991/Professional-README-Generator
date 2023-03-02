39ab8e8e94140edc078aae12663c4d6e0ebd722b
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-03-02 19:35:47 +0000

    added questions to index.js, installed inquirer

--- index.js
+++ index.js
@@ -4,9 +4,76 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+inquirer .prompt([
+    {
+        type:"input",
+        message:"Please input the title of your project:",
+        name: "project",
+    },
+    {
+        type: "input",
+        message: "Please provide a description of your project:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Does anything need to be installed?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "How is the app used?",
+        name: "useage",
+    },
+    {
+        type: "list",
+        message: "What was the app built with?",
+        name: "built",
+        choices: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Node.Js",
+            "JQuery",
+            "React",
+        ],
+    },
+    {
+        type: "input",
+        message: "Please provide your email address:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Please provide a link to your Github:",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Please provide a link to your LinkedIn:",
+        name: "linkedin"
+    },
+    {
+        type: "list", 
+        message: "What license is required for this project?",
+        name: "license",
+        choices: [
+            "GNU General Public License v3.0",
+            "MIT License",
+            "BSD 2-Clause 'Simplified' License",
+            "BSD 3-Clause 'New' or 'Revised License",
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU Affero General Public License v3.0",
+            "GNU General Public License v2.0",
+            "GNU Lesser General Public License v2.1",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ]
+    }
+]);
 
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
